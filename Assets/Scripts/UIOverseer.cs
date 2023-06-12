a1b30cd91bb7d75e9e7e7ba207c3fcfb6258a68b
JonathanN28 <98481216+JonathanN28@users.noreply.github.com>
2023-06-12 09:05:03 -0400

    restarbut

--- Assets/Scripts/UIOverseer.cs
+++ Assets/Scripts/UIOverseer.cs
@@ -7,14 +7,15 @@ using UnityEngine.SceneManagement;
 public class UIOverseer : MonoBehaviour
 {
     public GameObject playerBall;
-    private Overseer Overseer;
     public TextMeshProUGUI scoreText;
     public GameObject scoreTextBackground;
     public GameObject restartButton;
+
+    public GameObject mouseTracker;
     // Start is called before the first frame update
     void Start()
     {
-        Overseer.GetComponent<Overseer>();
+      
     }
 
     // Update is called once per frame
@@ -27,20 +28,20 @@ public class UIOverseer : MonoBehaviour
     {
         if (win)
         {
+            restartButton.SetActive(false);
             scoreText.text = scoreText.text + "\n" + "You Win!";
             scoreTextBackground.transform.localScale = new Vector2(2, 2);
-            restartButton.SetActive(true);
         }
         else
         {
+            restartButton.SetActive(false);
             scoreText.text = scoreText.text + "\n" + "Game Over!";
             scoreTextBackground.transform.localScale = new Vector2(2, 2);
             Cursor.visible = true;
-            restartButton.SetActive(true);
         }
     }
-    public static void Restart()
+    public void Restart()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(0);
     }
 }
